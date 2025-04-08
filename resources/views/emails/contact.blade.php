6bd103d4b6a8d09925eb0b568091d802cb9f72d0
wjhucleo <laachermariam@gmail.com>
2025-04-08 10:57:10 +0100

    fix bug email

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -1,7 +1,7 @@
 <h2>Vous avez reçu un nouveau message :</h2>
 
-<p><strong>Nom et Prénom :</strong> {{ $name }}</p>
-<p><strong>Email :</strong> {{ $email }}</p>
-<p><strong>Téléphone :</strong> {{ $phone }}</p>
-<p><strong>Message :</strong></p>
-<p>{{ $message }}</p>
+<p><strong>Nom :</strong> {{ $data['name'] }}</p>
+<p><strong>Email :</strong> {{ $data['email'] }}</p>
+<p><strong>Téléphone :</strong> {{ $data['phone'] }}</p>
+<p><strong>Message :</strong> {{ $data['message'] }}</p>
+
